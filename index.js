305cd08f82149173bb33eb174c685f8ff912216c
Michael Dear <michaelcdear@gmail.com>
2022-06-28 17:59:26 -0600

    commit tree c247346abbb74b845ccbf8b22ac570d26a08180d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards (array, event) {
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(`Thank you, ${element}, for the wonderful surprise gift!`)
+    });
+    return newArray
+}
+
+function countDown (integer) {
+    for(let i = integer; i >= 0; i--) {
+        console.log(i)
+    }
+}
+
+countDown(10)
